2e061bf8d42bf18c18d51ede46296cc8a0733b8e
Saerom Park <park.srome@gmail.com>
2019-09-14 22:19:14 -0500

    create notifications & set duration

--- background.js
+++ background.js
@@ -22,7 +22,9 @@ chrome.contextMenus.onClicked.addListener(info => {
     chrome.storage.local.get([storageKey], result => {
       let updatedNote;
       if (result.storageKey) {
+        console.log('there is a storage key');
         updatedNote = result.storageKey + '\n' + note;
+        console.log('updateNote')
       } else {
         updatedNote = note;
       }
@@ -32,8 +34,38 @@ chrome.contextMenus.onClicked.addListener(info => {
         console.log('@@@ key: @@@');
         console.log(storageKey);
         console.log('@@@ value: @@@');
-        console.log(note);
+        console.log(updatedNote);
+        
+        chrome.notifications.create({
+          type: 'basic',
+          iconUrl: './images/389cd119.png',
+          title: 'Note Buddy',          
+          message: 'Successfully saved note ✿',
+        }, () => {
+          console.log('@@@ created notification @@@');
+          
+          console.log('$$$ creating alarm now $$$')
+          chrome.alarms.create(
+            'notificationClearAlarm',  
+            { when: Date.now() + 1400 }
+          );
+        });
       });
     });
+  });
+});
+
+chrome.alarms.onAlarm.addListener(() => {
+  console.log('@@@ in onAlarm listener @@@');
+  
+  chrome.notifications.getAll(notifications => {
+    console.log('@@@ notifications: @@@');
+    console.log(notifications);
+
+    for (let id in notifications) {
+      chrome.notifications.clear(id, (wasCleared) => {
+        console.log(`@@@ notification was cleared: ${wasCleared} @@@`)
+      });
+    }
   });
 });
