32b9615968ddadc617c56451c11c5c9ffb9385c2
Saerom Park <park.srome@gmail.com>
2019-09-17 23:24:53 -0500

    add hotkey

--- background.js
+++ background.js
@@ -1,71 +1,60 @@
 chrome.runtime.onInstalled.addListener(() => {
-  
   chrome.contextMenus.create({
     id: 'randomID',
     contexts: ['selection'],
     title: 'save note ฅ^•ﻌ•^ฅ',
-  }, () => {
-    console.log('@@@ "save note" context menu item created @@@');
   });
-
 });
 
 chrome.contextMenus.onClicked.addListener(info => {
-  console.log('@@@ context menu clicked @@@');
   const note = info.selectionText;
+  saveNote(note);
+});
 
+chrome.commands.onCommand.addListener(function (command) {
+  if (command === 'save-note') {
+    chrome.tabs.executeScript({
+      code: 'window.getSelection().toString();'
+    }, selection => {
+      const note = selection[0];
+      saveNote(note);
+    });
+  }
+});
+
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.notifications.getAll(notifications => {
+    for (let id in notifications) {
+      chrome.notifications.clear(id);
+    }
+  });
+});
+
+function saveNote(note) {
   chrome.identity.getProfileUserInfo(userInfo => {
-    console.log('@@@ grabbed profile user @@@');
     const email = userInfo.email;
     const storageKey = email + ':notebuddy';
-
     chrome.storage.local.get([storageKey], result => {
       let updatedNote;
       if (result.storageKey) {
-        console.log('there is a storage key');
         updatedNote = result.storageKey + '\n' + note;
-        console.log('updateNote')
       } else {
         updatedNote = note;
       }
-
       chrome.storage.local.set({ [storageKey]: updatedNote }, () => {
-        console.log('@@@ stored to local storage: @@@');
-        console.log('@@@ key: @@@');
-        console.log(storageKey);
-        console.log('@@@ value: @@@');
-        console.log(updatedNote);
-        
         chrome.notifications.create({
           type: 'basic',
           iconUrl: './images/389cd119.png',
-          title: 'Note Buddy',          
+          title: 'Note Buddy',
           message: 'Successfully saved note ✿',
         }, () => {
-          console.log('@@@ created notification @@@');
-          
-          console.log('$$$ creating alarm now $$$')
+          const notificationDuration = 1400;
           chrome.alarms.create(
-            'notificationClearAlarm',  
-            { when: Date.now() + 1400 }
+            'notificationClearAlarm',
+            { when: Date.now() + notificationDuration }
           );
         });
       });
     });
   });
-});
-
-chrome.alarms.onAlarm.addListener(() => {
-  console.log('@@@ in onAlarm listener @@@');
-  
-  chrome.notifications.getAll(notifications => {
-    console.log('@@@ notifications: @@@');
-    console.log(notifications);
-
-    for (let id in notifications) {
-      chrome.notifications.clear(id, (wasCleared) => {
-        console.log(`@@@ notification was cleared: ${wasCleared} @@@`)
-      });
-    }
-  });
-});
+}
